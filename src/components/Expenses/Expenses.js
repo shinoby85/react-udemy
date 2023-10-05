db43d251ee8ce6a4edf65b359811d26d288d8e9b
Zhenya Pokratsyeu <eponkratyev@tula.co>
2023-10-05 18:11:18 +0300

    Finished task 2

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,12 +1,21 @@
 import './Expenses.css'
 import ExpenseItem from "./ExpenseItem";
+import ExpensesFilter from "./ExpensesFilter";
+import {useState} from "react";
 
 function Expenses(props) {
+    const [filterYear, setFilterYear] = useState(2021);
+
     return (
         <div className='expenses'>
-            {(props.expenses.map(item => (
-                <ExpenseItem id={item.id} title={item.title} amount={item.amount} date={item.date} />
-            )))}
+            <div className="expenses-filter">
+                <ExpensesFilter updateFilterYear={setFilterYear} />
+            </div>
+            <div className="expenses-items">
+                {(props.expenses.filter(item => item.date.getFullYear() == filterYear || !filterYear).map(item => (
+                    <ExpenseItem id={item.id} title={item.title} amount={item.amount} date={item.date}/>
+                )))}
+            </div>
         </div>
     )
 }
