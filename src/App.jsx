e776d5f1e3dfd597b712779591b59f02266a9df7
Evgeny Ponkratyev <eponkratyev@tula.co>
2023-11-10 00:28:21 +0300

    Create Player component

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,17 @@
-function App() {
+import Player from "./components/Player.jsx";
 
-    return (
-        <></>
-    )
+function App() {
+  
+  return (
+    <main>
+      <div id="game-container">
+        <ol id="players">
+          <Player initialName={"Player 1"} symbol={"X"}/>
+          <Player initialName={"Player 2"} symbol={"O"}/>
+        </ol>
+      </div>
+    </main>
+  )
 }
 
 export default App
---
+++ src/components/Player.jsx
@@ -0,0 +1,24 @@
+import {useState} from "react";
+
+export default function Player({initialName, symbol}) {
+  const [playerName, setPlayerName] = useState(initialName);
+  const [isEditing, setIsEditing] = useState(false);
+  let editablePlayerName = isEditing ? (<input type="text" required value={playerName} onChange={handleChange}/>) : (
+    <span className="player-name">{playerName}</span>);
+  
+  function handleChange(ev) {
+    setPlayerName(ev.target.value)
+  }
+  
+  function handleEditClick() {
+    setIsEditing(editing => !editing);
+  }
+  
+  return (<li>
+       <span className="player">
+         {editablePlayerName}
+         <span className="player-symbol">{symbol}</span>
+       </span>
+      <button onClick={() => handleEditClick()}>{!isEditing ? 'Edit' : 'Save'}</button>
+    </li>);
+}
