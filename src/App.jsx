5cb0e4e9e513e26374bf1fbf8949db4bd3815643
Evgeny Ponkratyev <eponkratyev@tula.co>
2023-12-06 18:11:41 +0300

    Create Result component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,29 @@
 import Header from "./components/Header.jsx";
 import './index.css'
 import UserInput from "./components/UserInput.jsx";
+import Result from "./components/Result.jsx";
+import {useState} from "react";
 
 function App() {
+  const [userInput, setUserInput] = useState({
+    initialInvestment: 10000,
+    annualInvestment: 1200,
+    expectedReturn: 6,
+    duration: 10
+  });
+  function handleChange(inputIdentifier, newValue) {
+    setUserInput(prevUserInput => {
+      return {
+        ...prevUserInput,
+        [inputIdentifier]: newValue
+      }
+    })
+  }
   return (
     <main>
       <Header />
-      <UserInput />
+      <UserInput userInput={userInput} onChange={handleChange}/>
+      <Result userInput={userInput} />
     </main>
 
   )
---
+++ src/components/Result.jsx
@@ -0,0 +1,13 @@
+export default function Result({userInput}) {
+  return (
+    <table id="result">
+      <thead>
+      <td className="center"></td>
+      <td className="center"></td>
+      <td className="center"></td>
+      <td className="center"></td>
+      </thead>
+      <tbody></tbody>
+    </table>
+  );
+}
