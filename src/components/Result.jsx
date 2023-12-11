46637aa29c8ba6d4ac15d1217fe2e741d112b8f9
Evgeny Ponkratyev <eponkratyev@tula.co>
2023-12-11 23:52:41 +0300

    Finish app

--- src/components/Result.jsx
+++ src/components/Result.jsx
@@ -1,13 +1,35 @@
+import {calculateInvestmentResults, formatter} from "../util/investment.js";
+
 export default function Result({userInput}) {
+  const resultData = calculateInvestmentResults(userInput);
+  const initialInvestment = resultData[0].valueEndOfYear - resultData[0].interest - resultData[0].annualInvestment;
   return (
     <table id="result">
       <thead>
-      <td className="center"></td>
-      <td className="center"></td>
-      <td className="center"></td>
-      <td className="center"></td>
+      <tr>
+        <th className="center">Year</th>
+        <th className="center">Investment Value</th>
+        <th className="center">Interest (year)</th>
+        <th className="center">Total Interest</th>
+        <th className="center">Invested Capital</th>
+      </tr>
       </thead>
-      <tbody></tbody>
+      <tbody>
+      {resultData.map(data => {
+        //Доход в текущем году
+        const totalInterest = data.valueEndOfYear - data.annualInvestment * data.year - initialInvestment;
+        //Всего инвестировано
+        const totalAmountInvested = data.valueEndOfYear - totalInterest;
+        return (
+          <tr key={data.year}>
+            <td>{data.year}</td>
+            <td>{formatter.format(data.valueEndOfYear)}</td>
+            <td>{formatter.format(data.interest)}</td>
+            <td>{formatter.format(totalInterest)}</td>
+            <td>{formatter.format(totalAmountInvested)}</td>
+          </tr>);
+      })}
+      </tbody>
     </table>
   );
 }
