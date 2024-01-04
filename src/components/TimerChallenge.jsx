1acb51059134d7bfc6e98bef0e0b285c2af9b847
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-01-05 00:58:23 +0300

    Refs

---
+++ src/components/TimerChallenge.jsx
@@ -0,0 +1,33 @@
+import {useRef, useState} from "react";
+
+export default function TimerChallenge({title, targetTimer}) {
+  const [timerStarted, setTimerStarted] = useState(false);
+  const [timerExpired, setTimerExpired] = useState(false);
+  const timer =useRef();
+
+  function handleStart() {
+    timer.current = setTimeout(() => {
+      setTimerExpired(true);
+    }, targetTimer * 1000);
+    setTimerStarted(true);
+  }
+  function handleStop() {
+    clearTimeout(timer.current);
+  }
+
+  return (
+    <section className='challenge'>
+      <h2>{title}</h2>
+      {timerExpired ?? <p>You lost!</p>}
+      <p className='challenge-time'>
+        {targetTimer} second{targetTimer > 1 ? 's' : ''}
+      </p>
+      <p>
+        <button onClick={handleStart}>{timerStarted ? "Stop" : "Start"} challenge</button>
+      </p>
+      <p className={timerStarted? 'active' : undefined}>
+        {timerStarted ? "Time is running..." : "Timer inactive"}
+      </p>
+    </section>
+  );
+}
