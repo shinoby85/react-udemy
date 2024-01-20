1b11a11240f890fcf403583db84c20f9189a9a8b
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-01-20 11:18:32 +0300

    Use dynamic styles

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -4,24 +4,16 @@ export default function Header() {
   const imgAltText = 'Stylized atom';
   const reactDescriptions = ['Fundamental', 'Crucial', 'Core'];
 
+  const isHighlight = true;
+
   function getRandomInt(max) {
     return Math.floor(Math.random() * (max + 1));
-  }
-
-  const titleStyles = {
-    fontFamily: '"Roboto Condensed", sans-serif',
-    fontSize: '5rem',
-    margin: '0',
-    background: 'linear-gradient(40deg, #ea00ff, #ea00ff, #03d5ff, #03d5ff)',
-    WebkitBackgroundClip: 'text',
-    WebkitTextFillColor: 'transparent',
-    filter: 'drop-shadow(0 2px 8px rgba(0, 0, 0, 0.5))'
   }
 
   return (
     <header>
       <img src={reactLogo} alt={imgAltText}/>
-      <h1 style={titleStyles}>React Essentials</h1>
+      <h1 className={`header__title ${isHighlight && 'highlight-text'} `}>React Essentials</h1>
       <p >
         {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are
         going to build!
