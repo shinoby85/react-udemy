6d51cd039752432a3255ecb26daace497b0d4c00
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-01-20 10:52:37 +0300

    Vanilla CSS

--- src/components/CoreConcept.jsx
+++ src/components/CoreConcept.jsx
@@ -1,19 +1,19 @@
 import './CoreConcept.css'
-export default function CoreConcept(props) {
-  return (
-    <li>
-      <img src={props.image} alt={props.title}/>
-      <h3>{props.title}</h3>
-      <p>{props.description}</p>
-    </li>
-  );
-}
-// function CoreConcept({image,title,description}) {
+// export default function CoreConcept(props) {
 //   return (
 //     <li>
-//       <img src={image} alt={title}/>
-//       <h3>{title}</h3>
-//       <p>{description}</p>
+//       <img src={props.image} alt={props.title}/>
+//       <h3>{props.title}</h3>
+//       <p>{props.description}</p>
 //     </li>
 //   );
 // }
+export default function CoreConcept({image,title,description}) {
+  return (
+    <li>
+      <img src={image} alt={title}/>
+      <h3>{title}</h3>
+      <p>{description}</p>
+    </li>
+  );
+}
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -9,10 +9,10 @@ export default function Header() {
   }
 
   return (
-    <header>
-      <img src={reactLogo} alt={imgAltText}/>
-      <h1>React Essentials</h1>
-      <p>
+    <header className='header'>
+      <img className="header__image" src={reactLogo} alt={imgAltText}/>
+      <h1 className="header__title">React Essentials</h1>
+      <p className="header__article">
         {reactDescriptions[getRandomInt(2)]} React concepts you will need for almost any app you are
         going to build!
       </p>
