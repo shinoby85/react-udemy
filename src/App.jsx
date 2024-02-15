41f5c6d617a84afb2a07d51a47194b1b62016f01
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-02-16 00:41:06 +0300

    Create new components and visible ProjectSideBar or NoProjectSelected

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,32 @@
 import ProjectSidebar from "./components/ProjectSidebar.jsx";
+import {useState} from "react";
+import NoProjectSelected from "./components/NoProjectSelected.jsx";
 import NewProject from "./components/NewProject.jsx";
 
 function App() {
+  const [projectState, setProjectState] = useState({
+    selectedProjectId: undefined,
+    projects: []
+  });
+  
+  function handleStartAddProject() {
+    setProjectState(prevState => ({
+      ...prevState,
+      selectedProjectId: null,
+    }))
+  }
+  
+  let content;
+  if (projectState.selectedProjectId === null) {
+    content = <NewProject/>;
+  } else if (projectState.selectedProjectId === undefined) {
+    content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
+  }
+  
   return (
     <main className="h-screen my-8 flex gap-8">
-      <ProjectSidebar/>
-      <NewProject/>
+      <ProjectSidebar onStartAddProject={handleStartAddProject}/>
+      {content}
     </ main>
   );
 }
