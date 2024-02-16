cb19214454e33e0bb123ec53050d5e4daf3936de
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-02-17 01:36:55 +0300

    Create project

--- src/App.jsx
+++ src/App.jsx
@@ -8,21 +8,37 @@ function App() {
     selectedProjectId: undefined,
     projects: []
   });
-  
+
   function handleStartAddProject() {
     setProjectState(prevState => ({
       ...prevState,
       selectedProjectId: null,
     }))
   }
-  
+
+  function handleAddProject(projectData) {
+    setProjectState(prevState => {
+      const newProject = {
+        ...projectData,
+        id: Math.random()
+      }
+      return {
+        ...prevState,
+        projects: [
+          ...prevState.projects,
+          newProject
+        ]
+      }
+    })
+  }
+
   let content;
   if (projectState.selectedProjectId === null) {
-    content = <NewProject/>;
+    content = <NewProject onAdd={handleAddProject}/>;
   } else if (projectState.selectedProjectId === undefined) {
     content = <NoProjectSelected onStartAddProject={handleStartAddProject}/>;
   }
-  
+
   return (
     <main className="h-screen my-8 flex gap-8">
       <ProjectSidebar onStartAddProject={handleStartAddProject}/>
