2429bcf6668065c05710374291cda6686a776656
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-04-02 00:18:56 +0300

    Add finish game message

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,20 +1,34 @@
 import {useState} from "react";
 import QUESTIONS from '../questions.js';
+import quizComplete from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswers, setUserAnswers] = useState([]);
   const activeQuestionIndex = userAnswers.length;
-
+  const quizIsComplete = QUESTIONS.length === activeQuestionIndex;
+  
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers(prevAnswer => ([...prevAnswer, selectedAnswer]));
   }
-
+  
+  if (quizIsComplete) {
+    return (
+      <div id="summary">
+        <img src={quizComplete}/>
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+  
+  const shuffledAnswer = [...QUESTIONS[activeQuestionIndex].answers];
+  shuffledAnswer.sort(() => Math.random() - 0.5);
+  
   return (
     <div id="quiz">
       <div id='questuon'>
         <h2>{QUESTIONS[activeQuestionIndex].text}</h2>
         <ul id="answers">
-          {QUESTIONS[activeQuestionIndex].answers.map((answer, index) => (
+          {shuffledAnswer.map((answer, index) => (
             <li key={answer} className="answer">
               <button onClick={() => handleSelectAnswer(answer)}>{answer}</button>
             </li>
