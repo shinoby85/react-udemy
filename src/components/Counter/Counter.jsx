4225a6a1edadb533558b13661fd3b226f7c06ff2
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-05-03 21:17:57 +0300

    New counter

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -7,56 +7,66 @@ import CounterOutput from './CounterOutput.jsx';
 import {log} from '../../log.js';
 
 function isPrime(number) {
-    log(
-        'Calculating if is prime number',
-        2,
-        'other'
-    );
-    if (number <= 1) {
-        return false;
+  log('Calculating if is prime number', 2, 'other');
+  
+  if (number <= 1) {
+    return false;
+  }
+  
+  const limit = Math.sqrt(number);
+  
+  for (let i = 2; i <= limit; i++) {
+    if (number % i === 0) {
+      return false;
     }
-
-    const limit = Math.sqrt(number);
-
-    for (let i = 2; i <= limit; i++) {
-        if (number % i === 0) {
-            return false;
-        }
-    }
-
-    return true;
+  }
+  
+  return true;
 }
 
 const Counter = memo(function Counter({initialCount}) {
-    log('<Counter /> rendered', 1);
-    const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
+  log('<Counter /> rendered', 1);
+  
+  const initialCountIsPrime = useMemo(
+    () => isPrime(initialCount),
+    [initialCount]
+  );
+  
+  // const [counter, setCounter] = useState(initialCount);
+  const [counterChanges, setCounterChanges] = useState([initialCount]);
+  
+  const currentCounter = counterChanges.reduce(
+    (prevCounter, counterChange) => prevCounter + counterChange,
+    0
+  );
+  
+  const handleDecrement = useCallback(function handleDecrement() {
+    // setCounter((prevCounter) => prevCounter - 1);
+    setCounterChanges((prevCounterChanges) => [-1, ...prevCounterChanges]);
+  }, []);
+  
+  const handleIncrement = useCallback(function handleIncrement() {
+    // setCounter((prevCounter) => prevCounter + 1);
+    setCounterChanges((prevCounterChanges) => [1, ...prevCounterChanges]);
+  }, []);
+  
+  return (
+    <section className="counter">
+      <p className="counter-info">
+        The initial counter value was <strong>{initialCount}</strong>. It{' '}
+        <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
+      </p>
+      <p>
+        <IconButton icon={MinusIcon} onClick={handleDecrement}>
+          Decrement
+        </IconButton>
+        <CounterOutput value={currentCounter}/>
+        <IconButton icon={PlusIcon} onClick={handleIncrement}>
+          Increment
+        </IconButton>
+      </p>
+    </section>
+  );
+});
 
-    const [counter, setCounter] = useState(initialCount);
-
-    const handleDecrement = useCallback(function handleDecrement() {
-        setCounter((prevCounter) => prevCounter - 1);
-    }, [])
-
-    const handleIncrement = useCallback(function handleIncrement() {
-        setCounter((prevCounter) => prevCounter + 1);
-    }, [])
-
-    return (
-        <section className="counter">
-            <p className="counter-info">
-                The initial counter value was <strong>{initialCount}</strong>. It{' '}
-                <strong>is {initialCountIsPrime ? 'a' : 'not a'}</strong> prime number.
-            </p>
-            <p>
-                <IconButton icon={MinusIcon} onClick={handleDecrement}>
-                    Decrement
-                </IconButton>
-                <CounterOutput value={counter}/>
-                <IconButton icon={PlusIcon} onClick={handleIncrement}>
-                    Increment
-                </IconButton>
-            </p>
-        </section>
-    );
-})
 export default Counter;
