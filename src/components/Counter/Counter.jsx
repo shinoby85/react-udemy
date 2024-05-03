800233dc4cc3f96f5d219cf7e17dcdab0c87d067
eponkratyev <eponkratyev@tula.co>
2024-05-03 16:26:36 +0300

    Use useMemo hook

--- src/components/Counter/Counter.jsx
+++ src/components/Counter/Counter.jsx
@@ -1,4 +1,4 @@
-import {memo, useCallback, useState} from 'react';
+import {memo, useCallback, useMemo, useState} from 'react';
 
 import IconButton from '../UI/IconButton.jsx';
 import MinusIcon from '../UI/Icons/MinusIcon.jsx';
@@ -29,7 +29,7 @@ function isPrime(number) {
 
 const Counter = memo(function Counter({initialCount}) {
     log('<Counter /> rendered', 1);
-    const initialCountIsPrime = isPrime(initialCount);
+    const initialCountIsPrime = useMemo(() => isPrime(initialCount), [initialCount]);
 
     const [counter, setCounter] = useState(initialCount);
 
