8a5175acdb8d8ecae22a371a62e5207859624c23
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-05-18 01:17:51 +0300

    Get data from api

--- src/components/AvailablePlaces.jsx
+++ src/components/AvailablePlaces.jsx
@@ -1,10 +1,17 @@
 import Places from './Places.jsx';
+import {useEffect, useState} from "react";
 
-export default function AvailablePlaces({ onSelectPlace }) {
+export default function AvailablePlaces({onSelectPlace}) {
+  const [availablePlaces, setAvailablePlaces] = useState([]);
+  useEffect(() => {
+    fetch('http://localhost:3000/places')
+      .then(response => response.json())
+      .then(resData => setAvailablePlaces(resData.places));
+  }, []);
   return (
     <Places
       title="Available Places"
-      places={[]}
+      places={availablePlaces}
       fallbackText="No places available."
       onSelectPlace={onSelectPlace}
     />
