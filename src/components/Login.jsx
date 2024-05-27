ae4b8d6ac6c0b3cadb3ce445fd4fa30b42c82619
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-05-27 23:15:51 +0300

    Work with form

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,20 +1,25 @@
 export default function Login() {
+  function handleSubmit(e) {
+    e.preventDefault();
+    console.log('Submitted!');
+  }
+  
   return (
-    <form>
+    <form onSubmit={handleSubmit}>
       <h2>Login</h2>
-
+      
       <div className="control-row">
         <div className="control no-margin">
           <label htmlFor="email">Email</label>
-          <input id="email" type="email" name="email" />
+          <input id="email" type="email" name="email"/>
         </div>
-
+        
         <div className="control no-margin">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"/>
         </div>
       </div>
-
+      
       <p className="form-actions">
         <button className="button button-flat">Reset</button>
         <button className="button">Login</button>
