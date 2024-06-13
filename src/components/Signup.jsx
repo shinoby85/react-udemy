2284c69f2523e8df89abdaebe11b0367f88c2574
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-06-13 23:36:58 +0300

    Use formik (add validators)

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,28 +1,69 @@
-export default function Signup() {
-  function handleSubmit(event) {
-    event.preventDefault();
-    const fd = new FormData(event.target);
-    const acquisitionChanel = fd.getAll('acquisition');
-    const data = Object.fromEntries(fd.entries());
-    data.acquisition = acquisitionChanel;
-    console.log(data);
-    event.target.reset();
+import {useFormik} from "formik";
+import {hasMinLength, isEmail} from "../util/validation.js";
+
+const validate = value => {
+  const validationFields = ['email', 'password', 'confirm-password'];
+  const errors = {};
+  
+  if (!value.email) {
+    errors.email = "Email is required";
+  } else if (!isEmail(value.email)) {
+    errors.email = "Email is not a valid";
   }
+  if (!value.password) {
+    errors.password = "Password is required";
+  } else if (hasMinLength(value.password, 6)) {
+    errors.password = "Password must be at least 6 characters";
+  }
+  if (!value['confirm-password']) {
+    errors['confirm-password'] = "Confirm password field is required";
+  } else if (hasMinLength(value['confirm-password'], 6)) {
+    errors['confirm-password'] = "Password must be at least 6 characters";
+  } else if (value.password !== value['confirm-password']) {
+    errors['confirm-password'] = "Passwords don't match";
+  }
+  return {
+    ...errors,
+    ...Object.fromEntries(validationFields.map(field => value[field] === '' ? [field, ''] : [field, errors[field]]))
+  };
+}
+export default function Signup() {
+  const formik = useFormik({
+    initialValues: {
+      email: '',
+      password: '',
+      ['confirm-password"']: '',
+      ['first-name']: '',
+      ['last-name']: '',
+      role: 'student',
+      acquisition: '',
+      terms: ''
+    },
+    validate,
+    onSubmit: (data) => {
+      console.log(data);
+    }
+  });
+  
   
   return (
-    <form onSubmit={handleSubmit}>
+    <form onSubmit={formik.handleSubmit}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
       
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email"/>
+        <input id="email" type="email" name="email" onChange={formik.handleChange} value={formik.values.email}
+               onBlur={formik.handleBlur}/>
+        {formik.errors.email && formik.touched.email ? <div>{formik.errors.email}</div> : null}
       </div>
       
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password"/>
+          <input id="password" type="password" name="password" onChange={formik.handleChange}
+                 value={formik.values.password} onBlur={formik.handleBlur}/>
+          {formik.errors.password && formik.touched.password ? <div>{formik.errors.password}</div> : null}
         </div>
         
         <div className="control">
@@ -31,7 +72,13 @@ export default function Signup() {
             id="confirm-password"
             type="password"
             name="confirm-password"
+            onChange={formik.handleChange}
+            value={formik.values['confirm-password']}
+            onBlur={formik.handleBlur}
           />
+          {formik.errors['confirm-password'] && formik.touched['confirm-password'] ?
+            <div>{formik.errors['confirm-password']}</div> : null}
+        
         </div>
       </div>
       
@@ -40,18 +87,21 @@ export default function Signup() {
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name"/>
+          <input type="text" id="first-name" name="first-name" onChange={formik.handleChange}
+                 value={formik.values['first-name']} onBlur={formik.handleBlur}/>
         </div>
         
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name"/>
+          <input type="text" id="last-name" name="last-name" onChange={formik.handleChange}
+                 value={formik.values['last-name']} onBlur={formik.handleBlur}/>
         </div>
       </div>
       
       <div className="control">
         <label htmlFor="phone">What best describes your role?</label>
-        <select id="role" name="role">
+        <select id="role" name="role" onChange={formik.handleChange} value={formik.values['role']}
+                onBlur={formik.handleBlur}>
           <option value="student">Student</option>
           <option value="teacher">Teacher</option>
           <option value="employee">Employee</option>
@@ -68,6 +118,8 @@ export default function Signup() {
             id="google"
             name="acquisition"
             value="google"
+            onChange={formik.handleChange}
+            checked={formik.values['acquisition'] === 'google'} onBlur={formik.handleBlur}
           />
           <label htmlFor="google">Google</label>
         </div>
@@ -78,19 +130,23 @@ export default function Signup() {
             id="friend"
             name="acquisition"
             value="friend"
+            onChange={formik.handleChange}
+            checked={formik.values['acquisition'] === 'friend'} onBlur={formik.handleBlur}
           />
           <label htmlFor="friend">Referred by friend</label>
         </div>
         
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other"/>
+          <input type="checkbox" id="other" name="acquisition" value="other" onChange={formik.handleChange}
+                 checked={formik.values['acquisition'] === 'other'} onBlur={formik.handleBlur}/>
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
       
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms"/>I
+          <input type="checkbox" id="terms-and-conditions" name="terms" onChange={formik.handleChange}
+                 value={formik.values.terms} onBlur={formik.handleBlur}/>I
           agree to the terms and conditions
         </label>
       </div>
