cb549e935247bf00f601c5d9a69b782deb65fb64
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-07-26 00:52:05 +0300

    Http post request

--- src/components/Meals.jsx
+++ src/components/Meals.jsx
@@ -1,24 +1,24 @@
-import {useEffect, useState} from "react";
 import MealItem from "./MealItem.jsx";
+import useHttp from "../hooks/useHttp.js";
 
+const requestConfig = {};
 export default function Meals() {
-  const [loadedMeals, setLoadedMeals] = useState([]);
+  const {
+    data: loadedMeals,
+    isLoading,
+    error
+  } = useHttp('http://localhost:3000/meals', requestConfig, []);
   
-  useEffect(() => {
-    (async function fetchMeals() {
-      const response = await fetch("http://localhost:3000/meals");
-      if (!response.ok) {
-      }
-      const meals = await response.json();
-      setLoadedMeals(meals);
-    })();
-  }, []);
+  if (isLoading) {
+    return <p>Fetching meals...</p>
+  }
   
   
-  return <ul id="meals">
-    {loadedMeals.length
-      ? loadedMeals.map(meal => (<MealItem key={meal.id} meal={meal}/>))
-      : <li>Loading...</li>
-    }
-  </ul>;
+  return (
+    <ul id="meals">
+      {loadedMeals.map((meal) => (
+        <MealItem key={meal.id} meal={meal}/>
+      ))}
+    </ul>
+  );
 }
