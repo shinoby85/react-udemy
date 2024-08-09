335a3c1aed88f5a7929696ec79d6f738e7441b63
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-08-09 23:34:46 +0300

    Redux for React applications (use action for state)

--- src/components/Counter.js
+++ src/components/Counter.js
@@ -1,7 +1,8 @@
 import classes from './Counter.module.css';
-import {useSelector} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 
 const Counter = () => {
+  const dispatch = useDispatch();
   const count = useSelector(state => state.counter);
   const toggleCounterHandler = () => {
   };
@@ -10,6 +11,10 @@ const Counter = () => {
     <main className={classes.counter}>
       <h1>Redux Counter</h1>
       <div className={classes.value}>{count}</div>
+      <div>
+        <button onClick={() => dispatch({type: 'increment'})}>Increment</button>
+        <button onClick={() => dispatch({type: 'decrement'})}>Decrement</button>
+      </div>
       <button onClick={toggleCounterHandler}>Toggle Counter</button>
     </main>
   );
