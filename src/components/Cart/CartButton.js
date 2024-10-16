a52a5d9292847f62e774d938eaf8e889b9a44451
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-10-17 00:14:46 +0300

    Add redux toolkit

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,12 +1,18 @@
 import classes from './CartButton.module.css';
+import {useDispatch} from "react-redux";
+import {uiActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
-  return (
-    <button className={classes.button}>
-      <span>My Cart</span>
-      <span className={classes.badge}>1</span>
-    </button>
-  );
+    const dispatch = useDispatch();
+    const toggleCartHandller = () => {
+        dispatch(uiActions.toggle());
+    }
+    return (
+        <button className={classes.button} onClick={toggleCartHandller}>
+            <span>My Cart</span>
+            <span className={classes.badge}>1</span>
+        </button>
+    );
 };
 
 export default CartButton;
---
+++ src/store/index.js
@@ -0,0 +1,11 @@
+import {configureStore} from "@reduxjs/toolkit";
+import uiReducers from "./ui-slice";
+
+const store = configureStore({
+    reducer: {
+        ui: uiReducers,
+        cart: null
+    }
+});
+
+export default store;
