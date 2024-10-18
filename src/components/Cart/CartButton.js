84ef5eee2fc5cc4e485c8afd93667b51802bbac5
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-10-19 00:30:46 +0300

    Update all project

--- src/components/Cart/CartButton.js
+++ src/components/Cart/CartButton.js
@@ -1,16 +1,17 @@
 import classes from './CartButton.module.css';
-import {useDispatch} from "react-redux";
+import {useDispatch, useSelector} from "react-redux";
 import {uiActions} from "../../store/ui-slice";
 
 const CartButton = (props) => {
     const dispatch = useDispatch();
+    const totalQuantity = useSelector(state => state.cart.totalQuantity);
     const toggleCartHandller = () => {
         dispatch(uiActions.toggle());
     }
     return (
         <button className={classes.button} onClick={toggleCartHandller}>
             <span>My Cart</span>
-            <span className={classes.badge}>1</span>
+            <span className={classes.badge}>{totalQuantity}</span>
         </button>
     );
 };
--- src/components/Cart/CartItem.js
+++ src/components/Cart/CartItem.js
@@ -1,8 +1,16 @@
 import classes from './CartItem.module.css';
+import {useDispatch} from "react-redux";
+import {cartActions} from "../../store/cart-slice";
 
 const CartItem = (props) => {
   const { title, quantity, total, price } = props.item;
-
+  const dispatch = useDispatch();
+const removeItemHandler = () => {
+  dispatch(cartActions.removeItemFromCart(props.item.id));
+}
+const addItemToCartHandler = () => {
+  dispatch(cartActions.addItemToCart(props.item));
+}
   return (
     <li className={classes.item}>
       <header>
@@ -17,8 +25,8 @@ const CartItem = (props) => {
           x <span>{quantity}</span>
         </div>
         <div className={classes.actions}>
-          <button>-</button>
-          <button>+</button>
+          <button onClick={removeItemHandler}>-</button>
+          <button onClick={addItemToCartHandler}>+</button>
         </div>
       </div>
     </li>
