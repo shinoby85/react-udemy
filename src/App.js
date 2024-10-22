7f7ec2530adc30f5e7b495c31e51ae77e02c31f4
eponkratyev <eponkratyev@tula.co>
2024-10-22 15:24:59 +0300

    Add http

--- src/App.js
+++ src/App.js
@@ -3,9 +3,19 @@ import {useSelector} from 'react-redux';
 import Cart from './components/Cart/Cart';
 import Layout from './components/Layout/Layout';
 import Products from './components/Shop/Products';
+import {useEffect} from "react";
 
 function App() {
   const showCart = useSelector((state) => state.ui.cartIsVisible);
+  const cart = useSelector((state) => state.cart);
+
+  useEffect(() => {
+    fetch('https://react-dummy-6d7c9-default-rtdb.firebaseio.com/cart.json', {
+      method: 'PUT',
+      body: JSON.stringify(cart),
+    })
+  }, [cart]);
+
 
   return (
     <Layout>
