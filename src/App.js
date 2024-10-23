89a471d3a264f0d21b7fb46e2131fe8a298034d9
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-10-23 22:04:09 +0300

    Create own action for api

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ import Products from './components/Shop/Products';
 import {Fragment, useEffect} from "react";
 import {uiActions} from "./store/ui-slice";
 import Notification from "./components/UI/Notification";
+import {sendCartData} from "./store/cart-slice";
 
 let isInit = true;
 
@@ -16,38 +17,11 @@ function App() {
   const notification = useSelector((state) => state.ui.notification);
 
   useEffect(() => {
-    const sendCartData = async () => {
-      dispatch(uiActions.showNotification({
-        status: 'pending',
-        title: 'Sending...',
-        message: 'Sending cart data!'
-      }))
-      const response = await fetch('https://react-dummy-6d7c9-default-rtdb.firebaseio.com/cart.json', {
-        method: 'PUT',
-        body: JSON.stringify(cart),
-      })
-      if (!response.ok) {
-        throw new Error('Sending cart data failed.')
-      }
-      const responseData = await response.json();
-      dispatch(uiActions.showNotification({
-        status: 'success',
-        title: 'Success',
-        message: 'Sent cart data successfully'
-      }))
-    }
     if (isInit) {
       isInit = false;
       return;
     }
-    sendCartData().catch(err => {
-      dispatch(uiActions.showNotification({
-        status: 'error',
-        title: 'Error!',
-        message: 'Sending cart data failed.'
-      }));
-    })
-
+    dispatch(sendCartData(cart));
   }, [cart, dispatch]);
 
 
