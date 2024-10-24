5ade50aadd4e89a5a3ad7a2ffc142a9137a08e75
eponkratyev <eponkratyev@tula.co>
2024-10-24 15:45:25 +0300

    Fix problem with cart

--- src/store/cart-actions.js
+++ src/store/cart-actions.js
@@ -11,8 +11,11 @@ export const fetchCartData = () => {
       return await response.json();
     }
     try {
-      const responseData = await getRequest();
-      dispatch(cartActions.replaceCart(responseData));
+      const cartData = await getRequest();
+      dispatch(cartActions.replaceCart({
+        items: cartData.items || [],
+        totalQuantity: cartData.totalQuantity || 0,
+      }));
       dispatch(uiActions.showNotification({
         status: 'success',
         title: 'Success',
@@ -38,7 +41,10 @@ export const sendCartData = (cart) => {
     const sendRequest = async () => {
       const response = await fetch('https://react-dummy-6d7c9-default-rtdb.firebaseio.com/cart.json', {
         method: 'PUT',
-        body: JSON.stringify(cart),
+        body: JSON.stringify({
+          items: cart.items,
+          totalQuantity: cart.totalQuantity || 0,
+        }),
       })
       if (!response.ok) {
         throw new Error('Sending cart data failed.');
