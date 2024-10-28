301802021c1d68bd43bdd57bea20d1a56f834537
eponkratyev <eponkratyev@tula.co>
2024-10-28 14:18:23 +0300

    Init navigate

--- src/App.js
+++ src/App.js
@@ -1,26 +1,17 @@
-import {createBrowserRouter, createRoutesFromElements, Route, RouterProvider} from "react-router-dom";
+import {createBrowserRouter, RouterProvider} from "react-router-dom";
 import HomePage from "./pages/HomePage";
 import ProductsPage from "./pages/ProductsPage";
 
-// const router = createBrowserRouter([
-//   {
-//     path: "/",
-//     element: <HomePage/>
-//   },
-//   {
-//     path: "/products",
-//     element: <ProductsPage/>
-//   }
-// ]);
-
-const routeDefinition = createRoutesFromElements(
-  <Route>
-    <Route path="/" element={<HomePage/>}/>
-    <Route path="/products" element={<ProductsPage/>}/>
-  </Route>
-);
-
-const router = createBrowserRouter(routeDefinition);
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <HomePage/>
+  },
+  {
+    path: "/products",
+    element: <ProductsPage/>
+  }
+]);
 
 function App() {
 
