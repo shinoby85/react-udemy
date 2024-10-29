a252cf42113d14dc31a62e2d26943695f6263493
eponkratyev <eponkratyev@tula.co>
2024-10-29 23:06:59 +0300

    Init navigate

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,6 +1,13 @@
+import {Link} from "react-router-dom";
+
 function HomePage() {
   return (
-    <h1>Home page</h1>
+    <>
+      <h1>Home page</h1>
+      <p>
+        Go to <Link to="/products">the list of product</Link>
+      </p>
+    </>
   );
 }
 
