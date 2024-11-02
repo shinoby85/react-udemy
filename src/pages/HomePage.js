b035289898c5826869d28b444a0451a3f3bce010
eponkratyev <eponkratyev@tula.co>
2024-11-02 13:04:44 +0300

    Usu navigating programmatically

--- src/pages/HomePage.js
+++ src/pages/HomePage.js
@@ -1,11 +1,22 @@
-import {Link} from "react-router-dom";
+import {Link, useNavigate} from "react-router-dom";
 
 function HomePage() {
+  const navigate = useNavigate();
+
+  function navigateHandler() {
+    setTimeout(() => {
+      navigate("/products");
+    }, 1500);
+  }
+
   return (
     <>
       <h1>Home page</h1>
       <p>
         Go to <Link to="/products">the list of product</Link>
+      </p>
+      <p>
+        <button onClick={navigateHandler}>Navigate</button>
       </p>
     </>
   );
