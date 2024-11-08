9b885e474ba3651c514593af682a889507e3c96d
eponkratyev <eponkratyev@tula.co>
2024-11-08 11:45:46 +0300

    Move loading fn

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,18 +1,17 @@
-import { useEffect, useState } from 'react';
-
 import EventsList from '../components/EventsList';
 import {useLoaderData} from "react-router-dom";
 
 function EventsPage() {
-  const events = useLoaderData();
-  return (
-     <EventsList events={events} />
-  );
+    const events = useLoaderData();
+    return (
+        <EventsList events={events}/>
+    );
 }
 
 export default EventsPage;
+
 export async function loading() {
-  const response = await fetch('http://localhost:8080/events');
-  const resData = await response.json();
-  return resData.events;
+    const response = await fetch('http://localhost:8080/events');
+    const resData = await response.json();
+    return resData.events;
 }
