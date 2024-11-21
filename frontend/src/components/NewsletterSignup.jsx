e31a09eef5a13b327d10ace06610be8e67fa2278
eponkratyev <eponkratyev@tula.co>
2024-11-21 17:57:31 +0300

    use defer

--- frontend/src/components/NewsletterSignup.jsx
+++ frontend/src/components/NewsletterSignup.jsx
@@ -1,8 +1,15 @@
 import classes from './NewsletterSignup.module.css';
 import {useFetcher} from "react-router-dom";
+import {useEffect} from "react";
 
 function NewsletterSignup() {
   const fetcher = useFetcher();
+  const {data, state} = fetcher;
+  useEffect(() => {
+    if (state === "idle" && data && data.message) {
+      window.alert(data.message);
+    }
+  }, [data, state]);
   return (
     <fetcher.Form method="post" action="/newsletter" className={classes.newsletter}>
       <input
--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,22 +1,33 @@
 import EventsList from '../components/EventsList';
-import {json, useLoaderData} from "react-router-dom";
+import {Await, defer, json, useLoaderData} from "react-router-dom";
+import {Suspense} from "react";
 
 function EventsPage() {
-    const data = useLoaderData();
-    const events = data.events;
-    return (
-        <EventsList events={events}/>
-    );
+  const {events} = useLoaderData();
+  return (
+    <Suspense fallback={<div style={{textAlign: 'center'}}>Loading...</div>}>
+      <Await resolve={events}>
+        {(loadEvents) => <EventsList events={loadEvents}/>}
+      </Await>
+    </Suspense>
+  );
 }
 
 export default EventsPage;
 
+async function loadEvents() {
+  const response = await fetch('http://localhost:8080/events');
+  if (!response.ok) {
+    // throw new Response(JSON.stringify({message: "Could not fetch events."}), {status: 500});
+    return json({message: "Could not fetch events."}, {status: 500});
+  } else {
+    const respData = await response.json();
+    return respData.events;
+  }
+}
+
 export async function loading() {
-    const response = await fetch('http://localhost:8080/events');
-    if (!response.ok) {
-        // throw new Response(JSON.stringify({message: "Could not fetch events."}), {status: 500});
-        return json({message: "Could not fetch events."}, {status: 500});
-    } else {
-        return response;
-    }
+  return defer({
+    events: loadEvents(),
+  });
 }
