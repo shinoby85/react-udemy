8d6a6e7a78fec832b929495801f574cf942a83c4
eponkratyev <eponkratyev@tula.co>
2024-11-28 00:17:34 +0300

    Logout

--- frontend/src/pages/Root.js
+++ frontend/src/pages/Root.js
@@ -1,16 +1,29 @@
-import { Outlet, useNavigation } from 'react-router-dom';
+import {Outlet, useSubmit} from 'react-router-dom';
 
 import MainNavigation from '../components/MainNavigation';
+import {getAuthToken} from "../util/auth";
+import {useEffect} from "react";
 
 function RootLayout() {
   // const navigation = useNavigation();
+  const token = getAuthToken();
+  const submit = useSubmit();
+
+  useEffect(() => {
+    if (!token) {
+      return null;
+    }
+    setTimeout(() => {
+      submit(null, {action: '/logout', method: 'POST'});
+    }, 60 * 60 * 1000)
+  }, [token, submit]);
 
   return (
     <>
-      <MainNavigation />
+      <MainNavigation/>
       <main>
         {/* {navigation.state === 'loading' && <p>Loading...</p>} */}
-        <Outlet />
+        <Outlet/>
       </main>
     </>
   );
