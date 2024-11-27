b6b597fcfceeac3da2a1f17d9d7837ddae5ee6ff
eponkratyev <eponkratyev@tula.co>
2024-11-28 01:17:53 +0300

    New project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+
+import BlogPage, { loader as postsLoader } from './pages/Blog';
+import HomePage from './pages/Home';
+import PostPage, { loader as postLoader } from './pages/Post';
+import RootLayout from './pages/Root';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <RootLayout />,
+    children: [
+      {
+        index: true,
+        element: <HomePage />,
+      },
+      {
+        path: 'posts',
+        children: [
+          { index: true, element: <BlogPage />, loader: postsLoader },
+          { path: ':id', element: <PostPage />, loader: postLoader },
+        ],
+      },
+    ],
+  },
+]);
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  return <RouterProvider router={router} />;
 }
 
 export default App;
