a30f828d3bdbdf27974d8aef0d6f63477b71f126
eponkratyev <eponkratyev@tula.co>
2024-11-27 20:00:25 +0300

    Logout (update)

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,12 +12,15 @@ import {action as manipulateEventAction} from './components/EventForm';
 import NewsletterPage, {action as newsletterAction} from './pages/Newsletter';
 import AuthenticationPage, {action as authAction} from "./pages/Authentication";
 import {action as logoutAction} from './pages/Logout';
+import {loaderToken} from "./util/auth";
 
 const router = createBrowserRouter([
   {
     path: '/',
     element: <RootLayout/>,
     errorElement: <ErrorPage/>,
+    id: 'root',
+    loader: loaderToken,
     children: [
       {index: true, element: <HomePage/>},
       {
