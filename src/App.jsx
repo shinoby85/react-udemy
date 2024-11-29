8a2988197b2dd8975e9cb3120ca2066c1f0787bf
eponkratyev <eponkratyev@tula.co>
2024-11-29 16:54:44 +0300

    Use useQuery from tanstack

--- src/App.jsx
+++ src/App.jsx
@@ -1,44 +1,45 @@
-import {
-  Navigate,
-  RouterProvider,
-  createBrowserRouter,
-} from 'react-router-dom';
+import {createBrowserRouter, Navigate, RouterProvider,} from 'react-router-dom';
 
 import Events from './components/Events/Events.jsx';
 import EventDetails from './components/Events/EventDetails.jsx';
 import NewEvent from './components/Events/NewEvent.jsx';
 import EditEvent from './components/Events/EditEvent.jsx';
+import {QueryClient, QueryClientProvider} from "@tanstack/react-query";
 
 const router = createBrowserRouter([
   {
     path: '/',
-    element: <Navigate to="/events" />,
+    element: <Navigate to="/events"/>,
   },
   {
     path: '/events',
-    element: <Events />,
+    element: <Events/>,
 
     children: [
       {
         path: '/events/new',
-        element: <NewEvent />,
+        element: <NewEvent/>,
       },
     ],
   },
   {
     path: '/events/:id',
-    element: <EventDetails />,
+    element: <EventDetails/>,
     children: [
       {
         path: '/events/:id/edit',
-        element: <EditEvent />,
+        element: <EditEvent/>,
       },
     ],
   },
 ]);
 
+const queryClient = new QueryClient();
+
 function App() {
-  return <RouterProvider router={router} />;
+  return <QueryClientProvider client={queryClient}>
+    <RouterProvider router={router}/>
+  </QueryClientProvider>
 }
 
 export default App;
