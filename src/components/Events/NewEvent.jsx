34af8036919b85fff41957d9cacc14970acff9de
Evgeny Ponkratyev <eponkratyev@tula.co>
2024-12-07 22:14:34 +0300

    useMutation

--- src/components/Events/NewEvent.jsx
+++ src/components/Events/NewEvent.jsx
@@ -1,24 +1,43 @@
-import { Link, useNavigate } from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
 
 import Modal from '../UI/Modal.jsx';
 import EventForm from './EventForm.jsx';
+import {useMutation} from "@tanstack/react-query";
+import {createNewEvent} from "../../util/http.js";
+import ErrorBlock from "../UI/ErrorBlock.jsx";
 
 export default function NewEvent() {
   const navigate = useNavigate();
+  const {mutate, isPending, isError, error} = useMutation({
+    mutationFn: createNewEvent
+  });
 
-  function handleSubmit(formData) {}
+  function handleSubmit(formData) {
+    mutate({
+      events: formData
+    })
+  }
 
   return (
     <Modal onClose={() => navigate('../')}>
       <EventForm onSubmit={handleSubmit}>
-        <>
-          <Link to="../" className="button-text">
-            Cancel
-          </Link>
-          <button type="submit" className="button">
-            Create
-          </button>
-        </>
+        {isPending && "Submitting..."}
+        {!isPending && (
+          <>
+            <Link to="../" className="button-text">
+              Cancel
+            </Link>
+            <button type="submit" className="button">
+              Create
+            </button>
+          </>
+        )}
+        {isError && (
+          <ErrorBlock
+            title="Failed to create a new event."
+            message={error.info?.message ||
+              "Failed to create event. Please check your inputs and try again later."}/>
+        )}
       </EventForm>
     </Modal>
   );
