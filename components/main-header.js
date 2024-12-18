9a919b751e110ff326b1cc73d76ab1d6e6a3d461
eponkratyev <eponkratyev@tula.co>
2024-12-18 12:42:03 +0300

    Use Image component

--- components/main-header.js
+++ components/main-header.js
@@ -1,12 +1,14 @@
 import Link from "next/link";
 import logoImg from "@/assets/logo.png";
 import classes from "./main-header.module.css";
+import Image from "next/image";
 
 export default function MainHeader() {
   return (
     <header className={classes.header}>
       <Link href="/" className={classes.logo}>
-        <img src={logoImg.src} alt="A plate with food on it"/>
+        {/*<img src={logoImg.src} alt="A plate with food on it"/>*/}
+        <Image src={logoImg} alt="A plate with food on it" priority/>
         Next level food
       </Link>
       <nav className={classes.nav}>
