b4500ce03eaa09f1eee114fabb99f30b4d7ff712
eponkratyev <eponkratyev@tula.co>
2024-12-18 14:45:51 +0300

    Optimise components styles

---
+++ components/main-header/main-header.js
@@ -0,0 +1,26 @@
+import Link from "next/link";
+import logoImg from "@/assets/logo.png";
+import classes from "./main-header.module.css";
+import Image from "next/image";
+import MainHeaderBackground from "@/components/main-header/main-header-background";
+
+export default function MainHeader() {
+  return (
+    <>
+      <MainHeaderBackground/>
+      <header className={classes.header}>
+        <Link href="/public" className={classes.logo}>
+          {/*<img src={logoImg.src} alt="A plate with food on it"/>*/}
+          <Image src={logoImg} alt="A plate with food on it" priority/>
+          Next level food
+        </Link>
+        <nav className={classes.nav}>
+          <ul>
+            <li><Link href="/meals">Browse meals</Link></li>
+            <li><Link href="/community">Foodies community</Link></li>
+          </ul>
+        </nav>
+      </header>
+    </>
+  );
+}
