5d9598efdabf66740cf8219348f3b64c23df5e29
eponkratyev <eponkratyev@tula.co>
2024-12-23 14:31:10 +0300

    Add meal details page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,11 +1,29 @@
-import Link from "next/link";
+import classes from "./page.module.css"
+import Image from "next/image";
+import {getMeal} from "@/lib/meals";
 
 export default function MealDetailsPage({params}) {
+  const meal = getMeal(params.slug);
+  meal.instructions = meal.instructions.replace(/\n/g, '<br />');
   return (
-    <main>
-      <h1 style={{color: 'white', textAlign: 'center'}}>Meal Details Page</h1>
-      <p style={{textAlign: 'center'}}>{params.slug}</p>
-      <p style={{textAlign: 'center'}}><Link href="../">Back</Link></p>
-    </main>
+    <>
+      <header className={classes.header}>
+        <div className={classes.image}>
+          <Image src={meal.image} alt={meal.title} fill/>
+        </div>
+        <div className={classes.headerText}>
+          <h1>{meal.title}</h1>
+          <p className={classes.creator}>
+            by <a href={`mailto: ${meal.creator_email}`}>{meal.creator}</a>
+          </p>
+          <p className={classes.summary}>{meal.summary}</p>
+        </div>
+      </header>
+      <main>
+        <p className={classes.instructions} dangerouslySetInnerHTML={{__html: meal.instructions}}>
+
+        </p>
+      </main>
+    </>
   );
 }
