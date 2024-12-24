2e259ea1599d8019cb2103079f89b741fba2ca84
eponkratyev <eponkratyev@tula.co>
2024-12-24 16:21:37 +0300

    Add share page

--- app/meals/[slug]/page.js
+++ app/meals/[slug]/page.js
@@ -1,9 +1,13 @@
+import {notFound} from "next/navigation";
 import classes from "./page.module.css"
 import Image from "next/image";
 import {getMeal} from "@/lib/meals";
 
 export default function MealDetailsPage({params}) {
   const meal = getMeal(params.slug);
+  if (!meal) {
+    notFound();
+  }
   meal.instructions = meal.instructions.replace(/\n/g, '<br />');
   return (
     <>
