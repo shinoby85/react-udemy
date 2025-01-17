6dc761d9794c415b3d6669ffc9eaa5f16e5a4aa5
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-01-17 14:46:55 +0300

    Add data to mongoDB

--- pages/api/new-meetup.js
+++ pages/api/new-meetup.js
@@ -1,11 +1,9 @@
 import {MongoClient} from "mongodb";
 
-const MongoDB_CONNECT_PARAM = 'mongodb+srv://zoomstudyitstep:59ytu6cyNOrNcVC7@cluster0.sdyfn.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
+const MongoDB_CONNECT_PARAM = 'mongodb+srv://zoomstudyitstep:HuKACEetXL3p008o@cluster0.sdyfn.mongodb.net/meetups?retryWrites=true&w=majority&appName=Cluster0'
 export default async function hundler(req, res) {
   if (req.method === 'POST') {
     const data = req.body;
-    const {title, image, address, description} = data;
-
     const client = await MongoClient.connect(MongoDB_CONNECT_PARAM);
     const db = client.db();
     const meetupCollection = db.collection('meetups');
--- pages/new-meetup/index.js
+++ pages/new-meetup/index.js
@@ -1,8 +1,23 @@
 import NewMeetupForm from "../../components/meetups/NewMeetupForm";
+import {useRouter} from "next/router";
 
 export default function NewMeetup() {
-  function addMeetupPage(enteredMeetupData) {
-    console.log(enteredMeetupData);
+  const router = useRouter();
+
+  async function addMeetupPage(enteredMeetupData) {
+    const response = await fetch('/api/new-meetup', {
+      method: "POST",
+      body: JSON.stringify(enteredMeetupData),
+      headers: {
+        "Content-Type": "application/json",
+      }
+    });
+
+    const data = await response.json();
+    console.log(data);
+
+    router.push("/");
+
   }
 
   return (
