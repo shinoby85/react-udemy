00d2a0f49f8dc4011b872e1a8c17f6d01ff236fa
eponkratyev <eponkratyev@tula.co>
2025-01-21 13:15:04 +0300

    Add ErrorBoundary component

--- app/page.js
+++ app/page.js
@@ -1,13 +1,15 @@
 import UsePromiseDemo from "@/components/UsePromisesDemo";
 import fs from "node:fs/promises";
 import {Suspense} from "react";
+import ErrorBoundary from "@/components/ErrorBoundary";
 
 export default async function Home() {
-  const usersPromise = new Promise(resolve => {
+  const usersPromise = new Promise((resolve, reject) => {
     setTimeout(async () => {
       const data = await fs.readFile('dummy-db.json', 'utf-8');
       const users = JSON.parse(data);
-      resolve(users);
+      // resolve(users);
+      reject(new Error('Error!'));
     }, 2000)
 
   })
@@ -19,9 +21,11 @@ export default async function Home() {
       {/*  <RSCDemo/>*/}
       {/*</ClientDemo>*/}
       {/*<DataFetchingDemo/>*/}
-      <Suspense fallback={<p>Loading data...</p>}>
-        <UsePromiseDemo usersPromise={usersPromise}/>
-      </Suspense>
+      <ErrorBoundary fallback={<p>Something wrong...</p>}>
+        <Suspense fallback={<p>Loading data...</p>}>
+          <UsePromiseDemo usersPromise={usersPromise}/>
+        </Suspense>
+      </ErrorBoundary>
     </main>
   );
 }
