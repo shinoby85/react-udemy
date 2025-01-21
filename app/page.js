2975f37b148f994d4720e4b0b4098b8ada42d5c0
eponkratyev <eponkratyev@tula.co>
2025-01-21 11:26:49 +0300

    Add UsePromiseDemo

--- app/page.js
+++ app/page.js
@@ -1,13 +1,27 @@
-import DataFetchingDemo from "@/components/DataFetchingDemo";
+import UsePromiseDemo from "@/components/UsePromisesDemo";
+import fs from "node:fs/promises";
+import {Suspense} from "react";
 
-export default function Home() {
+export default async function Home() {
+  const usersPromise = new Promise(resolve => {
+    setTimeout(async () => {
+      const data = await fs.readFile('dummy-db.json', 'utf-8');
+      const users = JSON.parse(data);
+      resolve(users);
+    }, 2000)
+
+  })
+
   return (
     <main>
       {/*<RSCDemo/>*/}
       {/*<ClientDemo>*/}
       {/*  <RSCDemo/>*/}
       {/*</ClientDemo>*/}
-      <DataFetchingDemo/>
+      {/*<DataFetchingDemo/>*/}
+      <Suspense fallback={<p>Loading data...</p>}>
+        <UsePromiseDemo usersPromise={usersPromise}/>
+      </Suspense>
     </main>
   );
 }
