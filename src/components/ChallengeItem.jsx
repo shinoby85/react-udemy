839dc9ea6e6663643d2fcc6a81e3d59a893c0119
eponkratyev <eponkratyev@tula.co>
2025-01-22 18:18:55 +0300

    Animate with Framer Motion

--- src/components/ChallengeItem.jsx
+++ src/components/ChallengeItem.jsx
@@ -1,12 +1,9 @@
 import {useContext} from 'react';
+import {motion} from 'framer-motion';
 
 import {ChallengesContext} from '../store/challenges-context.jsx';
 
-export default function ChallengeItem({
-                                        challenge,
-                                        onViewDetails,
-                                        isExpanded,
-                                      }) {
+export default function ChallengeItem({challenge, onViewDetails, isExpanded}) {
   const {updateChallengeStatus} = useContext(ChallengesContext);
 
   const formattedDate = new Date(challenge.deadline).toLocaleDateString(
@@ -42,11 +39,14 @@ export default function ChallengeItem({
             </p>
           </div>
         </header>
-        <div className={`challenge-item-details ${isExpanded ? 'expanded' : ''}`}>
+        <div className="challenge-item-details">
           <p>
             <button onClick={onViewDetails}>
               View Details{' '}
-              <span className="challenge-item-details-icon">&#9650;</span>
+              <motion.span
+                className="challenge-item-details-icon"
+                animate={{rotate: isExpanded ? 180 : 0}}
+              >&#9650;</motion.span>
             </button>
           </p>
 
