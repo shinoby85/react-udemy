2a9b8c737e31e0c8167fa5e7f5bc9686c3acc40f
eponkratyev <eponkratyev@tula.co>
2025-01-27 12:56:12 +0300

    Trigger badge component

--- src/components/Badge.jsx
+++ src/components/Badge.jsx
@@ -1,3 +1,11 @@
-export default function Badge({ caption }) {
-  return <span className="badge">{caption}</span>;
+import {motion} from "framer-motion";
+
+export default function Badge({caption}) {
+  return <motion.span
+    animate={{scale: [1, 1.2, 1]}}
+    transition={{duration: 0.3}}
+    className="badge"
+  >
+    {caption}
+  </motion.span>;
 }
