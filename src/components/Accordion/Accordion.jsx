0ee8c6993dd5a5b8f1b45b86fe98d0ed4c369779
eponkratyev <eponkratyev@tula.co>
2025-01-28 23:44:11 +0300

    Accordion

---
+++ src/components/Accordion/Accordion.jsx
@@ -0,0 +1,35 @@
+import {createContext, useContext, useState} from "react";
+
+const AccordionContext = createContext();
+
+export function useAccordionContext() {
+  const ctx = useContext(AccordionContext);
+  if (!ctx) {
+    throw new Error('Accordion-related components must be wrapped by <Accordion>');
+  }
+  return ctx;
+}
+
+export default function Accordion({className, children}) {
+  const [openItemId, setOpenItemId] = useState(null);
+
+  function openItem(id) {
+    setOpenItemId(id);
+  }
+
+  function closeItem() {
+    setOpenItemId(null);
+  }
+
+  const contextValue = {
+    openItemId,
+    openItem,
+    closeItem
+  };
+  return (
+    <AccordionContext.Provider value={contextValue}>
+      <ul className={className}>
+        {children}
+      </ul>
+    </AccordionContext.Provider>)
+}
