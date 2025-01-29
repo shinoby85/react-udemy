12e55c41d687e9e6f6cb8c9736267b30465b8794
eponkratyev <eponkratyev@tula.co>
2025-01-29 12:33:35 +0300

    Use context for id

--- src/App.jsx
+++ src/App.jsx
@@ -5,10 +5,10 @@ function App() {
     <section>
       <h2>Why work with us?</h2>
       <Accordion className="accordion">
-        <Accordion.Item className="accordion-item">
-          <Accordion.Title id="experience" className="accordion-item-title">We got 20 years of
+        <Accordion.Item className="accordion-item" id="experience">
+          <Accordion.Title className="accordion-item-title">We got 20 years of
             experience</Accordion.Title>
-          <Accordion.Content className="accordion-item-content" id="experience">
+          <Accordion.Content className="accordion-item-content">
             <article>
               <p>You can&apos;t go wrong with us.</p>
               <p>We are in the business of planing highly individualized vacation trips for more then 20 years.</p>
@@ -16,10 +16,10 @@ function App() {
           </Accordion.Content>
 
         </Accordion.Item>
-        <Accordion.Item className="accordion-item">
-          <Accordion.Title id="local-guides" className="accordion-item-title">We are working with local
+        <Accordion.Item id="local-guides" className="accordion-item">
+          <Accordion.Title className="accordion-item-title">We are working with local
             guides.</Accordion.Title>
-          <Accordion.Content className="accordion-item-content" id="local-guides">
+          <Accordion.Content className="accordion-item-content">
             <article>
               <p>Lorem ipsum dolor sit amet.</p>
               <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Beatae deleniti impedit in molestiae quos
