e72f854d1b7c7ad5a9a0f5d5a3e203d43da93c2d
eponkratyev <eponkratyev@tula.co>
2025-01-29 14:51:57 +0300

    Debouncing

--- src/components/SearchableList/SearchableList.jsx
+++ src/components/SearchableList/SearchableList.jsx
@@ -1,10 +1,17 @@
-import {useState} from "react";
+import {useRef, useState} from "react";
 
 export default function SearchableList({items, itemKeyFn, children}) {
+  const lastChange = useRef();
   const [searchTerm, setSearchTerm] = useState('');
 
   function handleInputChange(evt) {
-    setSearchTerm(evt.target.value);
+    if (lastChange.current) {
+      clearTimeout(lastChange.current);
+    }
+    lastChange.current = setTimeout(() => {
+      lastChange.current = null;
+      setSearchTerm(evt.target.value);
+    }, 500);
   }
 
   const searchResult = items.filter((item) => JSON.stringify(item).toLowerCase().includes(searchTerm.toLowerCase()));
