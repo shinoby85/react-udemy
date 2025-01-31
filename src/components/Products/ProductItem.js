f93635c5794a30003da2dec24be7fc99444b6c4c
eponkratyev <eponkratyev@tula.co>
2025-01-31 15:13:33 +0300

    Toggling favorite

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,19 +1,19 @@
-import React from 'react';
-import { useDispatch } from 'react-redux';
+import React, {useContext} from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
-import { toggleFav } from '../../store/actions/products';
+import {ProductContext} from "../../context/products-context";
 
 const ProductItem = props => {
-  const dispatch = useDispatch();
+  const {toggleFav} = useContext(ProductContext);
 
   const toggleFavHandler = () => {
-    dispatch(toggleFav(props.id));
+    // dispatch(toggleFav(props.id));
+    toggleFav(props.id);
   };
 
   return (
-    <Card style={{ marginBottom: '1rem' }}>
+    <Card style={{marginBottom: '1rem'}}>
       <div className="product-item">
         <h2 className={props.isFav ? 'is-fav' : ''}>{props.title}</h2>
         <p>{props.description}</p>
--- src/containers/Products.js
+++ src/containers/Products.js
@@ -1,11 +1,12 @@
-import React from 'react';
-import { useSelector } from 'react-redux';
+import React, {useContext} from 'react';
 
 import ProductItem from '../components/Products/ProductItem';
 import './Products.css';
+import {ProductContext} from "../context/products-context";
 
 const Products = props => {
-  const productList = useSelector(state => state.shop.products);
+  // const productList = useSelector(state => state.shop.products);
+  const productList = useContext(ProductContext).products;
   return (
     <ul className="products-list">
       {productList.map(prod => (
