0600483777c7147f518bc49c717e0881a2ba76f4
eponkratyev <eponkratyev@tula.co>
2025-02-01 15:53:34 +0300

    optimize custom store

--- src/components/Products/ProductItem.js
+++ src/components/Products/ProductItem.js
@@ -1,11 +1,12 @@
-import React from 'react';
+import React, {memo} from 'react';
 
 import Card from '../UI/Card';
 import './ProductItem.css';
 import {useStore} from "../../hooks-store/store";
 
-const ProductItem = props => {
-  const dispatch = useStore()[1];
+const ProductItem = memo(props => {
+  console.log("Rendering")
+  const dispatch = useStore(false)[1];
   const toggleFavHandler = () => {
     dispatch("TOGGLE_FAV", props.id);
   };
@@ -24,6 +25,6 @@ const ProductItem = props => {
       </div>
     </Card>
   );
-};
+});
 
 export default ProductItem;
