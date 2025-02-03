16f88df2a7a4da3b2e705f4d7e03fc078000a8ff
eponkratyev <eponkratyev@tula.co>
2025-02-03 13:09:01 +0300

    Create new project

--- src/App.js
+++ src/App.js
@@ -1,20 +1,25 @@
-import React from 'react';
-import { Route } from 'react-router-dom';
+import logo from './logo.svg';
+import './App.css';
 
-import Navigation from './components/Nav/Navigation';
-import ProductsPage from './containers/Products';
-import FavoritesPage from './containers/Favorites';
-
-const App = props => {
+function App() {
   return (
-    <React.Fragment>
-      <Navigation />
-      <main>
-        <Route path="/" component={ProductsPage} exact />
-        <Route path="/favorites" component={FavoritesPage} />
-      </main>
-    </React.Fragment>
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
   );
-};
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -1,16 +1,7 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import {BrowserRouter} from 'react-router-dom';
+import ReactDOM from 'react-dom';
 
 import './index.css';
 import App from './App';
-import configureStore from "./hooks-store/products-store";
 
-configureStore();
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <BrowserRouter>
-    <App/>
-  </BrowserRouter>
-);
+ReactDOM.render(<App />, document.getElementById('root'));
