dbf595d48a73c267664cd222c92347b2fd1039e3
eponkratyev <eponkratyev@tula.co>
2025-02-05 20:54:44 +0300

    Add async test

--- src/components/Async.test.js
+++ src/components/Async.test.js
@@ -3,6 +3,10 @@ import {render, screen} from "@testing-library/react";
 
 describe("Async component tests", () => {
   test("renders posts if request success", async () => {
+    window.fetch = jest.fn();
+    window.fetch.mockResolvedValueOnce({
+      json: async () => [{id: 'p1', title: 'First post'}],
+    });
     render(<Async/>);
     const listItemElements = await screen.findAllByRole("listitem");
     expect(listItemElements).not.toHaveLength(0);
