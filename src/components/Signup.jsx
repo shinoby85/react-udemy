dfb56996430661c2cd1053f7eb36d73a9adbc8a5
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-14 19:51:27 +0300

    Add error messages

--- src/components/Signup.jsx
+++ src/components/Signup.jsx
@@ -1,18 +1,59 @@
+import {hasMinLength, isEmail, isEqualToOtherValue, isNotEmpty} from "../util/validation.js";
+
 export default function Signup() {
+  function signupAction(formData) {
+    const email = formData.get('email');
+    const password = formData.get('password');
+    const confirmPassword = formData.get('confirmPassword');
+    const firstName = formData.get('firstName');
+    const lastName = formData.get('lastName');
+    const role = formData.get('role');
+    const terms = formData.get('terms');
+    const acquisitionChannel = formData.getAll('acquisition');
+
+    let errors = [];
+
+    if (!isEmail(email)) {
+      errors.push('Invalid email address');
+    }
+
+    if (!isNotEmpty(password) || !hasMinLength(password, 6)) {
+      errors.push('You must provide a password with at least 6 characters long.');
+    }
+
+    if (!isEqualToOtherValue(password, confirmPassword)) {
+      errors.push('Passwords do not match');
+    }
+
+    if (!isNotEmpty(firstName) || !isNotEmpty(lastName)) {
+      errors.push('Please provide both first name and last name.');
+    }
+
+    if (!isNotEmpty(role)) {
+      errors.push('Please select a role');
+    }
+    if (!terms) {
+      errors.push('You must agree to the terms and conditions.');
+    }
+    if (acquisitionChannel.length === 0) {
+      errors.push('Please select a least one acquisition channel.');
+    }
+  }
+
   return (
-    <form>
+    <form action={signupAction}>
       <h2>Welcome on board!</h2>
       <p>We just need a little bit of data from you to get you started 🚀</p>
 
       <div className="control">
         <label htmlFor="email">Email</label>
-        <input id="email" type="email" name="email" />
+        <input id="email" type="email" name="email"/>
       </div>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="password">Password</label>
-          <input id="password" type="password" name="password" />
+          <input id="password" type="password" name="password"/>
         </div>
 
         <div className="control">
@@ -25,17 +66,17 @@ export default function Signup() {
         </div>
       </div>
 
-      <hr />
+      <hr/>
 
       <div className="control-row">
         <div className="control">
           <label htmlFor="first-name">First Name</label>
-          <input type="text" id="first-name" name="first-name" />
+          <input type="text" id="first-name" name="first-name"/>
         </div>
 
         <div className="control">
           <label htmlFor="last-name">Last Name</label>
-          <input type="text" id="last-name" name="last-name" />
+          <input type="text" id="last-name" name="last-name"/>
         </div>
       </div>
 
@@ -73,14 +114,14 @@ export default function Signup() {
         </div>
 
         <div className="control">
-          <input type="checkbox" id="other" name="acquisition" value="other" />
+          <input type="checkbox" id="other" name="acquisition" value="other"/>
           <label htmlFor="other">Other</label>
         </div>
       </fieldset>
 
       <div className="control">
         <label htmlFor="terms-and-conditions">
-          <input type="checkbox" id="terms-and-conditions" name="terms" />I
+          <input type="checkbox" id="terms-and-conditions" name="terms"/>I
           agree to the terms and conditions
         </label>
       </div>
