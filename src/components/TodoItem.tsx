9b0be4a77e00ba11700a3212f1c3af3a7f53ecb4
Evgeny Ponkratyev <eponkratyev@tula.co>
2025-02-14 18:42:40 +0300

    New project

--- src/components/TodoItem.tsx
+++
@@ -1,7 +0,0 @@
-import {FC, ReactNode} from "react";
-import classes from "./TodoItem.module.css";
-
-const TodoItem: FC<{ text: string, children?: ReactNode, onRemoveItem: () => void }> = ({text, onRemoveItem}) => {
-  return <li onClick={onRemoveItem} className={classes.item}>{text}</li>
-}
-export default TodoItem;
